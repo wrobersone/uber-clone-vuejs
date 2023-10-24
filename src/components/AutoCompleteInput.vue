50e1d9bea2664e3242a2986b97674910cdd1f363
William Robersone <william.robersone@gmail.com>
2023-10-24 17:38:35 -0500

    first commit

---
+++ src/components/AutoCompleteInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-center" id="AutoCompleteInput">
+    <input :id="theId" v-model="inputComputed" @click="emit('isActive', true)" type="text" :placeholder="placeholder" class="text-md w-full py-2.5 px-3 appearance-none bg-gray-100 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:bg-gray-200" autocomplete="off">
+    <WindowCloseIcon @click="emit('isActive', true)" fillColor="#2E2E2E" />
+  </div>
+</template>
+
+<script setup>
+import { toRefs, defineProps, defineEmits, computed } from 'vue'
+import WindowCloseIcon from "vue-material-design-icons/WindowClose.vue"
+
+const emit = defineEmits(['update:input', 'clearInput', 'isActive'])
+
+const props = defineProps({
+  theId: String,
+  input: String,
+  placeholder: String,
+})
+const { theId, input, placeholder } = toRefs(props)
+const inputComputed = computed({
+  get: () => input.value,
+  set: (val) => emit('update:input', val),
+})
+</script>
---
+++ src/views/MapView.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="MapView">
+    <div id="map"></div>
+
+    <div class="w-full" id="VehicleSelection">
+      <div class="w-full h-2 border-t"></div>
+      <div class="w-full text-center border-t-2 p-1.5 text-gray-700 text-lg font-semibold">Distance - 50</div>
+      <div class="scrollSection">
+        
+        <div class="bg-custom-gray">
+          <div class="flex items-center px-4 py-5">
+            <img src="img/uber/ride.png" alt="" width="75">
+            <div class="w-full ml-3">
+              <div class="flex items-center justify-between">
+                <div class="mb-1 text-2xl">UberX</div>
+                <div class="text-xl">$59.99</div>
+              </div>
+              <div class="text-gray-500">7 Hours</div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center px-4 py-5">
+            <img src="img/uber/comfort.png" alt="" width="75">
+            <div class="w-full ml-3">
+              <div class="flex items-center justify-between">
+                <div class="mb-1 text-2xl">UberX</div>
+                <div class="text-xl">$89.99</div>
+              </div>
+              <div class="text-gray-500">9 Hours</div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center px-4 py-5">
+            <img src="img/uber/uberxl.png" alt="" width="75">
+            <div class="w-full ml-3">
+              <div class="flex items-center justify-between">
+                <div class="mb-1 text-2xl">UberXL</div>
+                <div class="text-xl">$119.99</div>
+              </div>
+              <div class="text-gray-500">11 Hours</div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+
+      <div class="flex bg-white items-center justify-center py-6 px-4 w-full absolute bottom-0 shadow-inner">
+        <button class="bg-black py-4 px-4 text-white text-2xl rounded-sm w-full">
+          Confirm UberX
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue"
+
+onMounted(() => {
+  setTimeout(() => {
+    initMap()
+  }, 50)
+})
+
+const initMap = () => {
+  const map = new window.google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    fullscreenControl: false,
+    zoomControl: false,
+    streetViewControl: false,
+    mapTypeControl: false
+  });
+  console.log(map)
+}
+
+</script>
+
+<style lang="scss">
+#MapView {
+
+  #map {
+    width: 100%;
+    height: 45vh;
+    top: 0;
+    left: 0;
+  }
+
+  .gm-style-cc {
+    display: none;
+  }
+  .bg-custom-gray {
+    background-color: rgb(237, 237, 237);
+  }
+
+  #VehicleSelection {
+    .scrollSection {
+      height: calc(50vh - 120px);
+      position: absolute;
+      overflow-y: auto;
+      width: 100%;
+    }
+  }
+}
+</style>
